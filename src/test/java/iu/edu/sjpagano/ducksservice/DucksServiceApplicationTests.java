5a57d1e4870f4e86952234d78fa7dcdfb3ed5a1b
sjpagano <sampagano5@gmail.com>
2024-02-07 18:09:14 -0500

    Initial commit

---
+++ src/test/java/iu/edu/sjpagano/ducksservice/DucksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iu.edu.sjpagano.ducksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
